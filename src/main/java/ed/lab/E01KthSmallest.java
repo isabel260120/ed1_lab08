4b415ba23bf7c5a4622ceeabeadd690fcd4332c4
roisa <roisabel012@gmail.com>
2026-04-07 19:40:31 -0600

    Ejercicio01 KthSmallest

--- src/main/java/ed/lab/E01KthSmallest.java
+++ src/main/java/ed/lab/E01KthSmallest.java
@@ -1,9 +1,25 @@
 package ed.lab;
 
 public class E01KthSmallest {
-
+    private int count;
+    private int answer;
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+        count = 0;
+        answer = 0;
+        inOrder(root, k);
+        return answer;
+    }
+    private void inOrder(TreeNode<Integer> node, int k) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left, k);
+        count++;
+        if (count == k) {
+            answer = node.value;
+            return;
+        }
+        inOrder(node.right, k);
     }
 
 }
